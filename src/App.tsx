3ac5b3edb0f4e7849e0444e46454073cd1e5391d
K Harsh <harshnaidu2u@gmail.com>
2023-09-06 00:36:41 +0530

    routing, bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
+// import { NewNote } from './NewNotes';
+import { Container } from 'react-bootstrap';
 
 
 function App() {
 
   return (
     <>
-     
+     <Container className='my-3'>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        {/* <Route path='/new' element={<NewNote/>} /> */}
+        <Route path='/:id'>
+          <Route index element={<h1>show</h1>} />
+          <Route path='edit' element={<h1>edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to={"/"}/>} />
+      </Routes>
+    </Container>
     </>
   )
 }
